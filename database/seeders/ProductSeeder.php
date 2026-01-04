03dca6efe1b0a9bcafec72ab767a47d7bb3649a8
Shacoholic <nemanja.nikolic97@gmail.com>
2026-01-04 12:35:52 +0100

    Create Product, Cart, CartItem models, migrations and ProductSeeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+          Product::insert([
+            ['name' => 'T-shirt', 'price' => 19.99, 'stock_quantity' => 20, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Shoe', 'price' => 49.99, 'stock_quantity' => 15, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Shorts', 'price' => 14.99, 'stock_quantity' => 30, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Scarf', 'price' => 39.99, 'stock_quantity' => 10, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Cap', 'price' => 9.99, 'stock_quantity' => 50, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Pants', 'price' => 29.99, 'stock_quantity' => 25, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Socks', 'price' => 4.99, 'stock_quantity' => 100, 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
